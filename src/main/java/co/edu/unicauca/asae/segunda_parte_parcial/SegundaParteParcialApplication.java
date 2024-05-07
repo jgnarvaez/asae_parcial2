08ccf39797d28cabbc9d410b05d297efee9c8a2e
José Gregorio <108169866+jgnarvaez@users.noreply.github.com>
2024-05-07 08:38:07 -0500

    first commit

---
+++ src/main/java/co/edu/unicauca/asae/segunda_parte_parcial/SegundaParteParcialApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.asae.segunda_parte_parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaParteParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaParteParcialApplication.class, args);
+	}
+
+}
