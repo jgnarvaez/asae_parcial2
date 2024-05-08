ae0970e6861ece8fe72bcac8962a255c93be70e7
pbuesaquillo <51162785+pbuesaquillo@users.noreply.github.com>
2024-05-08 08:56:45 -0500

    querys agregadas

--- src/main/java/co/edu/unicauca/asae/segunda_parte_parcial/infraestructura/output/persistencia/repositorios/CuestionarioRepository.java
+++ src/main/java/co/edu/unicauca/asae/segunda_parte_parcial/infraestructura/output/persistencia/repositorios/CuestionarioRepository.java
@@ -1,9 +1,17 @@
 package co.edu.unicauca.asae.segunda_parte_parcial.infraestructura.output.persistencia.repositorios;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.unicauca.asae.segunda_parte_parcial.infraestructura.output.persistencia.entidades.CuestionarioEntity;
 
 public interface CuestionarioRepository extends CrudRepository<CuestionarioEntity, Integer>{
+
+    List<CuestionarioEntity> findByTituloStartingWithIgnoreCaseOrderByIdDesc(String titulo);
+
+    @Query("SELECT count(*) FROM Cuestionarios c WHERE c.titulo = :titulo")
+    Integer existeCuestionarioConTitulo(String titulo);
 
 }
--- src/main/java/co/edu/unicauca/asae/segunda_parte_parcial/infraestructura/output/persistencia/repositorios/DocenteRepository.java
+++ src/main/java/co/edu/unicauca/asae/segunda_parte_parcial/infraestructura/output/persistencia/repositorios/DocenteRepository.java
@@ -1,9 +1,13 @@
 package co.edu.unicauca.asae.segunda_parte_parcial.infraestructura.output.persistencia.repositorios;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import co.edu.unicauca.asae.segunda_parte_parcial.infraestructura.output.persistencia.entidades.DocenteEntity;
 
 public interface DocenteRepository extends CrudRepository<DocenteEntity, Integer>{
+
+    @Query("SELECT count(*) FROM personas p  WHERE p.correo = :correo")
+    Integer existeDocenteConCorreo(String correo);
 
 }
